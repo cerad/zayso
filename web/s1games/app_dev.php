436a314078f537f92d4beb5b03851b514274e9d0
ahundiak <ahundiak@spike.mshome.net>
2013-09-26 11:23:04 -0500

    S1Games Schedule/Results

---
+++ web/s1games/app_dev.php
@@ -0,0 +1,15 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+require_once __DIR__.'/../../../aysos1games/app/bootstrap.php.cache';
+Debug::enable();
+require_once __DIR__.'/../../../aysos1games/app/AppKernel.php';
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
