65a1b5dd4407212ad543e2f8b8d4884473ffdbcb
ahundiak <ahundiak@spike.mshome.net>
2013-10-29 16:17:23 -0500

    nafutsal app_prod

--- web/nafutsal/app_dev.php
+++ web/nafutsal/app_dev.php
@@ -4,9 +4,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
 require_once __DIR__.'/../../../nafutsal/app/bootstrap.php.cache';
+Debug::enable();
 require_once __DIR__.'/../../../nafutsal/app/AppKernel.php';
 
-$kernel = new AppKernel('prod', false);
+$kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
