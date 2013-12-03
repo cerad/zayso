72c1d01a6139e81cf7630e1ccacf262f94e8ca10
ahundiak <ahundiak@spike.mshome.net>
2013-12-03 10:26:38 -0600

    s1games20131203 branch

---
+++ web/s1games20131203/app.php
@@ -0,0 +1,17 @@
+<?php
+
+define('CERAD_TOURN_SHOW_CONFIG','s1');
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
+
+require_once __DIR__.'/../../../aysos1games20131203/app/bootstrap.php.cache';
+Debug::enable();
+require_once __DIR__.'/../../../aysos1games20131203/app/AppKernel.php';
+
+$kernel = new AppKernel('dev', true);
+$kernel->loadClassCache();
+$request = Request::createFromGlobals();
+$response = $kernel->handle($request);
+$response->send();
+$kernel->terminate($request, $response);
